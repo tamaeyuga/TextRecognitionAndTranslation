2b3547589a239c444654b2ff1ea583d4b545c930
YugaTamae <yuga69@icloud.com>
2020-08-04 16:01:46 +0900

    Initial Commit

---
+++ TextRecognitionAndTranslation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextRecognitionAndTranslation
+//
+//  Created by 玉栄友雅 on 2020/08/04.
+//  Copyright © 2020 Yuga Tamae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TextRecognitionAndTranslationTests/TextRecognitionAndTranslationTests.swift
@@ -0,0 +1,34 @@
+//
+//  TextRecognitionAndTranslationTests.swift
+//  TextRecognitionAndTranslationTests
+//
+//  Created by 玉栄友雅 on 2020/08/04.
+//  Copyright © 2020 Yuga Tamae. All rights reserved.
+//
+
+import XCTest
+@testable import TextRecognitionAndTranslation
+
+class TextRecognitionAndTranslationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
